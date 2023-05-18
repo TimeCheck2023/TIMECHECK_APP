416420bbf84c176c81ae9723166f134cea43c103
caro109 <carogarcia.0419@gmail.com>
2023-05-18 16:11:41 -0500

    agregando las card de las estadisticas

--- src/screens/Org/HomeOrg.jsx
+++ src/screens/Org/HomeOrg.jsx
@@ -1,12 +1,16 @@
 import { View, Text } from 'react-native'
 import React, { useContext } from 'react'
 import { AuthContext } from '../../context/AuthContext'
+import { VictoryChart, VictoryBar, VictoryTheme } from 'victory-native'
+import {Card} from 'react-native-paper'
 
 const HomeOrg = () => {
   const { logout } = useContext(AuthContext)
   return (
-    <View className='flex-1 justify-center items-center bg-slate-500'>
-      <Text>HomeOrg</Text>
+    <View className='flex-1 justify-center items-center bg-slate-100'>
+      <Card elevation={5}>
+        <Text>Sena</Text>
+      </Card>
     </View>
   )
 }
