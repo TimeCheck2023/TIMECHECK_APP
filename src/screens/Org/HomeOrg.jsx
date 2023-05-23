4b85dd881123f180e0f8472fbff749f0d6d5e959
caro109 <carogarcia.0419@gmail.com>
2023-05-23 17:47:56 -0500

    Se termino la card y se creo el componente donde ira las graficas

--- src/screens/Org/HomeOrg.jsx
+++ src/screens/Org/HomeOrg.jsx
@@ -1,19 +1,19 @@
-import { View, Text, TouchableOpacity, ImageBackground } from 'react-native'
-import React, { useContext } from 'react'
+import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext'
 
-const HomeOrg = ({items}) => {
-  const { logout } = useContext(AuthContext)
+const HomeOrg = ({ items }) => {
+  const { logout } = useContext(AuthContext);
   return (
     <View className='flex-1 justify-center items-center bg-slate-100'>
-      <TouchableOpacity className='w-96 h-72 z-30'>
-        <View className='w-80 h-72 z-30 border-solid rounded-md bg-purple-500 opacity-70 bg-opacity-50'>
-          <Text className='text-slate-950 text-center text-2xl font-bold'>Sena</Text>
-          <Text className='text-slate-950'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga assumenda voluptatum enim, consequuntur nihil autem esse asperiores error distinctio consequatur sit dolor quia suscipit, quo a exercitationem accusantium beatae recusandae!</Text>
+      <TouchableOpacity className='w-72 h-72 z-30'>
+        <View className="h-full w-full bg-purple-500 flex justify-around rounded-md shadow-md border border-gray-500">
+          <Text className='text-white text-center text-2xl font-bold'>Sena</Text>
+          <Text className='text-white text-justify px-8 space-y-4 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga assumenda voluptatum enim, consequuntur nihil autem esse asperiores error distinctio consequatur sit dolor quia suscipit, quo a exercitationem accusantium beatae recusandae!</Text>
         </View>
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default HomeOrg
+export default HomeOrg;
