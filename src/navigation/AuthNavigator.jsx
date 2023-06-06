a316685561fdc666ca299b14d63f8f2e7a55241f
danie aldana <danielaldana212@gmail.com>
2023-06-06 17:52:13 -0500

    cambiel el welcome principal y agregue una imagen de figma

--- src/navigation/AuthNavigator.jsx
+++ src/navigation/AuthNavigator.jsx
@@ -1,10 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Welcome from '../screens/Welcome';
 import Sign_up from '../screens/Sign_up'
 import Sign_In from '../screens/Sign_In';
 import VerificationScreen from '../screens/VerificationScreen';
-import Welcome2 from '../screens/Welcome2';
-
+import Welcome2 from '../screens/Welcome';
 const Stack = createNativeStackNavigator();
 
 
@@ -12,7 +10,7 @@ const AuthNavigator = () => {
    
     return (
         <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name='Welcome2' component={Welcome2} />
+            <Stack.Screen name='Welcome' component={Welcome2} />
             <Stack.Screen name='Sign_In' component={Sign_In} />
             <Stack.Screen name='Sign_Up' component={Sign_up} />
             <Stack.Screen name='VerificationScreen' component={VerificationScreen} />
--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -1,94 +1,84 @@
-import { View, Text, FlatList, Dimensions, TouchableOpacity, StyleSheet } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { useState } from 'react'
-import { AntDesign } from '@expo/vector-icons';
-import * as Animatable from "react-native-animatable";
-import { data } from "../utils/data";
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Dimensions, Image } from 'react-native'
+import React from 'react'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import fondo from '../../assets/image/fondo.jpg'
+
+const { width, height } = Dimensions.get('window');
 
 
-
-
-const { width, height } = Dimensions.get('window')
-
-const Welcome = ({ navigation }) => {
-
-  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-  const ref = React.useRef();
-  // console.log(height);
-  // console.log(width);
-  const updateSliderCurrentIndex = (e) => {
-    // toma o captura la posicion de la imagen
-    const contentOffSet = e.nativeEvent.contentOffset.x;
-    // para calcular el índice actual y lo redondea
-    const currentIndex = Math.round(contentOffSet / width);
-    setCurrentSlideIndex(currentIndex);
-  }
-
-  const handleNext = () => {
-    const nextSlideIndex = currentSlideIndex + 1;
-    if (nextSlideIndex != data.length) {
-      const offset = nextSlideIndex * width;
-      ref?.current.scrollToOffset({ offset });
-      setCurrentSlideIndex(currentSlideIndex + 1);
-    }
-  }
-
-  const CardWelcome = ({ item }) => {
+const Welcome2 = ({ navigation }) => {
     return (
-      <View className='flex-1 items-center w-screen' style={{ top: hp('10%') }}>
-        {/* <View className='w-screen sm:h-96  bg-slate-500' style={{width: wp('100%')}}> */}
-        <View className='rounded-3xl items-center justify-center' style={{ width: wp('90%'), height: hp('40%') }}>
-          <Animatable.Image animation='bounce' duration={1500} source={item.image} className='w-full h-full rounded-3xl' resizeMode='contain' />
+        <View style={{ flex: 1 }}>
+            <ImageBackground style={styles.container} source={fondo}>
+                <View style={styles.content}>
+                    <Image style={{ width: 370, height: 400, borderRadius: 20, resizeMode: 'cover' }} source={{ uri: 'https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }} />
+                    <Text style={{ color: 'white', fontSize: 28, fontWeight: 'bold', marginTop: 22 }}>Bienvenidos a timecheck!</Text>
+                    <Text style={styles.contentDescription}>
+                        TimeCheck es una herramienta en línea que facilita el registro automatizado de asistentes a eventos. Permite a los usuarios registrarse en línea
+                        {/* , a los organizadores monitorear la asistencia en tiempo real y generar informes de asistencia. Simplifica la gestión de eventos y mejora la experiencia tanto para los organizadores como para los participantes. */}
+                    </Text>
+                    <View style={{ flex: 1, flexDirection: 'row' }}>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => navigation.navigate('Sign_Up')}>
+                            <View style={styles.containerBtn}>
+                                <Text style={{ fontWeight: 'bold', fontSize: 16, color: '#6C63FF' }}>Register</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity activeOpacity={0.8} onPress={() => navigation.navigate('Sign_In')}>
+                            <View style={styles.containerBtn}>
+                                <Text style={{ fontWeight: 'bold', fontSize: 16, color: '#6C63FF' }}>Login</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ImageBackground>
         </View>
-        {/* </View> */}
-        <View className=''>
-          <Text className='text-center font-bold	dark:text-white' style={{ fontSize: hp('3.5%'), marginTop: hp('5%') }}>{item.title}</Text>
-          {/* <Text className='text-3xl text-center font-semibold	dark:text-white'>{item.title2}</Text> */}
-          <Text className='text-justify dark:text-white' style={{ width: wp('92%'), fontSize: hp('2.3%'), fontSize: wp('4.4%'), marginTop: hp('1%') }}>{item.description}</Text>
-        </View>
-      </View>
     )
-  }
-
-
-  return (
-    <View className='flex-1'>
-      <View className='flex-row pb-16 pl-10 pr-10 bg-[#6C63FF]' style={{
-        borderBottomRightRadius: 50,
-        height: hp('56%')
-      }}>
-      </View>
-      <View className='absolute top-0 bottom-0 left-0 right-0 items-center'>
-        <View className='flex-row h-4 items-center top-12'>
-          {
-            data.map((_, index) => {
-              return <View className={`w-7 h-[5] rounded-xl bg-white ml-1'`} key={index} />
-            })
-          }
-        </View>
-        <FlatList
-          ref={ref}
-          onMomentumScrollEnd={updateSliderCurrentIndex}
-          keyExtractor={item => item.id}
-          data={data}
-          renderItem={({ item }) => <CardWelcome item={item} />}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          pagingEnabled
-        />
-        <View className='flex-row  justify-between items-center rounded-2xl' style={{ width: wp('100%'), height: hp('8%'), bottom: hp('6') }}>
-          <TouchableOpacity activeOpacity={0.7} className={`bg-[#6C63FF] items-center justify-center left-6  rounded-lg`} style={{ width: wp('40%'), height: hp('8.2%') }} onPress={() => navigation.navigate('Sign_Up')} >
-            <Text className={`text-xl text-white font-bold`}>Register</Text>
-          </TouchableOpacity >
-          <TouchableOpacity activeOpacity={0.7} className={`bg-slate-400 items-center justify-center right-6 rounded-lg`} style={{ width: wp('40%'), height: hp('8.2%') }} onPress={() => navigation.navigate('Sign_In')}>
-            <Text className={`text-xl text-white font-bold`}>Login</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-    </View>
-  )
 }
 
-export default Welcome
+export default Welcome2
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: width,
+        height: height,
+        resizeMode: 'cover',
+    },
+    content: {
+        height: '95%',
+        width: '100%',
+        bottom: 0,
+        position: 'absolute',
+        paddingHorizontal: 20,
+        paddingVertical: 40,
+        // backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    },
+    contentDescription: {
+        color: 'white',
+        lineHeight: hp('3%'),
+        fontSize: hp('2%'),
+        fontSize: wp('4.5%'),
+        marginTop: hp('2%'),
+    },
+    containerBtn: {
+        width: 150,
+        height: 50,
+        backgroundColor: 'white',
+        marginTop: 60,
+        marginLeft: 20,
+        borderRadius: 7,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    containerBtn2: {
+        position: 'absolute',
+        width: 120,
+        height: 50,
+        backgroundColor: 'white',
+        borderRadius: 7,
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 140,
+        right: 25,
+    }
+})
